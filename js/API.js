a8492917542911ba35b29a6ade5f3e955c4bc78c
Cristianps1988 <cristianps_1@hotmail.com>
2023-04-16 22:10:31 -0500

    create, read, delete functions added

---
+++ js/API.js
@@ -0,0 +1,36 @@
+const url = 'http://localhost:4000/clientes'
+
+export const nuevoCliente = async cliente => {
+    try {
+        await fetch(url, {
+            method: 'POST',
+            body: JSON.stringify(cliente),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        window.location.href = 'index.html'
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const cargarClientes = async () => {
+    try {
+        const resultado  = await fetch(url)
+        const clientes = await resultado.json()
+        return clientes
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const eliminarCliente = async id => {
+    try {
+        await fetch(`${url}/${id}`, {
+            method: 'DELETE'
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
